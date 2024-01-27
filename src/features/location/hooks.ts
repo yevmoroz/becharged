9a2816dca3ede2d972f0e070fad04cf08d1aa861
Yev Moroz <yev.moroz.dev@gmail.com>
2024-01-27 10:23:12 -0500

    location refactor

--- src/features/location/hooks.ts
+++ src/features/location/hooks.ts
@@ -1,27 +1,44 @@
-import { useState, useEffect } from 'react';
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { useState, useEffect } from 'react';
 
-export const useDeviceLocation = () => {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+const getDeviceCoordinates = async () => {
+  const { status } = await requestForegroundPermissionsAsync();
+  if (status !== 'granted') {
+    throw new Error('Permission to access location was denied');
+  }
+
+  const location = await getCurrentPositionAsync();
+  if (!location?.coords) {
+    throw new Error('Location is not available');
+  }
+  return location.coords;
+};
+
+export type LocationDetails = {
+  latitude: number | null;
+  longitude: number | null;
+};
+
+export type OptionalLocationDetails = LocationDetails | null;
+type OptionalError = Error | null;
+
+export const useDeviceLocation = (): [OptionalLocationDetails, OptionalError] => {
+  const [location, setLocation] = useState<OptionalLocationDetails>(null);
+  const [error, setError] = useState<OptionalError>(null);
+  const noCoordinates = !location?.latitude || !location?.longitude;
 
   useEffect(() => {
-    (async () => {
+    if (noCoordinates) {
+      getDeviceCoordinates()
+        .then((l) =>
+          setLocation({
+            latitude: l.latitude,
+            longitude: l.longitude,
+          })
+        )
+        .catch(setError);
+    }
+  }, [noCoordinates]);
 
-      let { status } = await requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  return {
-    errorMsg,
-    lat: location?.coords?.latitude,
-    lon: location?.coords?.longitude,
-  }
-}
+  return [location, error];
+};
